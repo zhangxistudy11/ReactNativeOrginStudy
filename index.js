8dce7c04b0140f37a6d6d7bef15da6d73dda1bcd
张玺 <xiza@ctrip.com>
2018-12-24 15:52:58 +0800

    flatlist

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import MyListLitem from './CommonPart/Component/MyListItem';
+AppRegistry.registerComponent(appName, () => MyListLitem);
