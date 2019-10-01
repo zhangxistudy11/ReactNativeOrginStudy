0649b67a40f84418c6f8d7d7a37975e7a536098e
zhangxi <xiza@ctrip.com>
2019-10-01 22:26:22 +0800

    Ff

--- ES6Grammar/BasicGrammar.js
+++ ES6Grammar/BasicGrammar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableHighlight ,NavigatorIOS, FlatList, TouchableWithoutFeedback } from 'react-native';
 import Styles from '../CommonPart//Style/Styles';
+import ES6Zero from './ES6Zero'
 
 export default class BasicGrammar extends React.Component {
   render() {
@@ -19,12 +20,36 @@ export default class BasicGrammar extends React.Component {
   }
   _renderFlatListItem = (data) => {
     return (<View  style={Styles.itemViewStyle}>
-      <TouchableWithoutFeedback onPress={() => {  }}>
+      <TouchableWithoutFeedback onPress={() => { this.clickItem(data.index) }}>
         <View>
           <Text style={ [Styles.itemTestStyle,{color:'#333'},{fontFamily:'PingFangSC-Regular'},{fontSize:17} ]}>{data.item.key}</Text>
         </View>
       </TouchableWithoutFeedback>
     </View>)
+  }
+  clickItem(index){
+    // console.log(index)
+     if(index==0){
+         this.testZeroMethod();
+     }
+     else if(index ==1){
+        this.testOneMethod();
+     }
+    // else if(index ==2){
+    //     this.testTwoMethod();
+    //  }
+   // else if(index ==3){
+    //     this.testThreeMethod();
+    //  }else if(index ==4){
+    //     this.testFourMethod();
+    //  }
+  }
+  testZeroMethod= ()=>{
+    this.props.navigator.push({
+        component: ES6Zero,
+        title:'基本语法'
+      });
+    
   }
 }
 
---
+++ ES6Grammar/ES6Zero.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight, NavigatorIOS, FlatList, TouchableWithoutFeedback } from 'react-native';
+import Styles from '../CommonPart//Style/Styles';
+import { Button } from 'beeshell/dist/components/Button';
+
+export default class ES6Zero extends React.Component {
+    constructor(props) {
+        super(props);
+        this.timer = null;
+        this.person = {
+            name :'zx',
+            class:{
+                sex:'男',
+                age:18
+            }
+        }
+      }
+    render() {
+        return (
+            <View style={[Styles.container, { marginTop: 100 }]}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                    {this._renderEventView(0)}
+                    {this._renderEventView(1)}
+                    {this._renderEventView(2)}
+                </View>
+
+            </View>
+        );
+    }
+    _renderEventView = (code) => {
+        let str = ''
+        if(code == 0){
+            str = '开始计时'
+        }else if (code == 1){
+            str = '停止计时'
+        }else if (code == 2){
+            str = '新开计时'
+        }
+        return (
+            <View style={{ width: 60, height: 40, backgroundColor: 'red', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', }}>
+                <TouchableWithoutFeedback onPress={() => { this.touchEvent(code) }}>
+                    <View style={{}}>
+                        <Text style={[{ color: '#333' }, { fontFamily: 'PingFangSC-Regular' }, { fontSize: 14 }]}>{str}</Text>
+                    </View>
+                </TouchableWithoutFeedback>
+                <View>
+ 
+                </View>
+            </View>
+        )
+    }
+    touchEvent = (code) => {
+     
+        if (code === 0) {
+         console.log('点击' + new Date())
+        this.startUserTimer()
+        } else if (code === 1){
+            console.log('停止' + new Date())
+         console.log('停止计时')
+         this.clearUserTimer()
+        }else if (code === 2){
+            console.log('重新计时'+ new Date())
+            this.startUserTimer()
+         }
+    }
+    startUserTimer (){
+     console.log(this.person.class.sex)
+    }
+    clearUserTimer ()
+    {
+        const {class:{sex}}= this.person
+        console.log(sex)
+    }
+}
